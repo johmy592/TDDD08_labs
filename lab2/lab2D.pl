7693a447f4b49c25a51ca3a63bfe76f2c39c51c3
johmy592 <johmy592@student.liu.se>
2018-10-01 14:15:26 +0200

    Lab 2 seems to be working correctly

--- lab2/lab2D.pl
+++ lab2/lab2D.pl
@@ -3,50 +3,38 @@
 % set_union/3 computes the union between two sets
 
 set_union([],[],[]).
-set_union(S,[],S).
-set_union([],S,S).
-set_union([H1|T1], [H2|T2], [H1|TR]) :-
-    H1 @< H2,
-    set_union(T1, [H2|T2], TR).
-
-set_union([H1|T1], [H2|T2], [H2|TR]) :-
-    H2 @< H1,
-    set_union([H1|T1], T2, TR).
+set_union([H|T],[],[H|T]).
+set_union([],[H|T],[H|T]).
 
 set_union([H1|T1], [H1|T2], [H1|TR]) :-
     set_union(T1,T2,TR).
 
+set_union([H1|T1], [H2|T2], [H2|TR]) :-
+    set_union([H1|T1], T2, TR),
+    H2 @< H1.
+
+set_union([H1|T1], [H2|T2], [H1|TR]) :-
+    set_union(T1, [H2|T2], TR),
+    H1 @< H2.
 
 % set_intersection/3 computes the intersection between 2 sets
 
-set_intersect([],_,[]).
-set_intersect(_,[],[]).
+set_intersect([],[],[]).
+set_intersect([],[_H|_T],[]).
+set_intersect([_H|_T],[],[]).
 
 set_intersect([H|T1], [H|T2], [H|TR]) :-
     set_intersect(T1, T2, TR).
 
 set_intersect([H1|T1], [H2|T2], Res) :-
-    H1 @< H2,
-    set_intersect(T1, [H2|T2], Res).
+    set_intersect(T1, [H2|T2], Res),
+    H1 @< H2.
 
 set_intersect([H1|T1], [H2|T2], Res) :-
-    H2 @< H1,
-    set_intersect([H1|T1], T2, Res).
-% is_set
-is_set([],true).
-is_set([_],true).
-is_set([H1,H2|T], Res) :-
-    H1 @< H2,
-    is_set([H2|T], Res).
+    set_intersect([H1|T1], T2, Res),
+    H2 @< H1.
 
+% powerset/2
+p_set(Set, Res) :-
+    findall(X, set_union(Set, X, Set),Res).
 
-% powerset.... i dunno man
-p_help([],[[]]).
-p_help([H|T], [HR|TR]) :-
-    set_union(T2, T, T),
-    is_set(T2, true),
-    %length([H|T], L2),
-    %length([H|T2], L1),
-    %L2 >= L1, 
-    set_intersect([H|T2],[H|T],X),
-    p_help(T, TR).

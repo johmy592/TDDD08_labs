a4dedbfe088f7bcc36e72d62bdc9a96e06d47c85
Viktor Regard <vikre869@student.liu.se>
2018-10-02 15:55:36 +0200

    Added some comments to lab1

--- lab1/lab1A.pl
+++ lab1/lab1A.pl
@@ -1,5 +1,6 @@
 % Lab 1 TDDD08
 
+% Defining which persons has which characteristics
 beautiful(ulrika).
 beautiful(nisse).
 beautiful(peter).
@@ -16,21 +17,19 @@ man(peter).
 woman(ulrika).
 woman(bettan).
 
-% Find out if X likes Y
+% likes(Person1, Person2)
+% Person2 is a person that Person1 likes according to following preferences.
 likes(X, Y) :- man(X),woman(Y),beautiful(Y).
-
-
 likes(ulrika,Y) :- man(Y), rich(Y), kind(Y),likes(Y, ulrika).
-
 likes(ulrika,Y) :- man(Y), beautiful(Y), strong(Y), likes(Y, ulrika).
-
 likes(nisse,Y) :- woman(Y), likes(Y, nisse).
 
-
+% happy(Person)
+% True if Person is rich or the Person likes someone 
+% from opposite gender and that someone likes the Person back.
 happy(X) :- rich(X).
 happy(X) :- man(X), woman(Y), likes(X,Y), likes(Y,X). 
 happy(X) :- man(Y), woman(X), likes(X,Y), likes(Y,X).
-
 
 
 /*
--- lab1/lab1B.pl
+++ lab1/lab1B.pl
@@ -1,3 +1,5 @@
+% Lab 1B
+
 % Facts representing a graph
 connected(a, b).
 connected(a, c).
@@ -11,17 +13,17 @@ connected(e, g).
 connected(f, g).
 
 
-% Appending lists
-app([],L,L).
-app([H|T], L, [H|R]) :- app(T,L,R).
-
-% Checks for a path between two nodes
+% path(X, Y)
+% There is a path between node X and Y if X and Y is connected
+% or if X and Y has connected nodes between them.
 path(X, Y) :- connected(X, Y).
 path(X, Y) :- connected(X, Z), path(Z, Y).
 
-% Checks for a path and saves the path
+% path(X, Y, Path)
+% Same as path(X, Y)/2, where Path is the list of nodes in the path.
 path(X, Y, [X,Y]) :- connected(X,Y).
 path(X, Y, [X|T]) :- connected(X,Z), path(Z,Y,T).
 
-% Checks for a path and saves the length of the path
+% npath(X, Y, L)
+% X and Y is two nodes in a graph where L is the length of the path between them.
 npath(X,Y,L) :- path(X,Y,[X|T]), length(T,L).
